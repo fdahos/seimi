fe805c7ab6029e092c3073528079d1c509de2e62
fdahos <131457311+fdahos@users.noreply.github.com>
2023-12-29 12:03:13 +0900

    Create display.js

---
+++ js/display.js
@@ -0,0 +1,16 @@
+setInterval(() => {
+  let instant = new Date();
+  let time = document.getElementById("now-time");
+  let event = document.getElementById("now-event");
+  let schedule = document.getElementById("schedule");
+  time.innerHTML = `${instant.getHours()}時${instant.getMinutes()}分${instant.getSeconds()}秒`;
+  event.innerHTML = now_event();
+  schedule.innerHTML = "";
+  for (let i = 0; i < past_event().length; i++) {
+    schedule.innerHTML += `<li class="${
+      past_event()[i][0]
+    }"><span class="event-time">${past_event()[i][2]}<br></span>${
+      past_event()[i][1]
+    }</li>`;
+  }
+}, 500);
